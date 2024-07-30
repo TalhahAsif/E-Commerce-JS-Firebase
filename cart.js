6eb4b649efdc2186f8b474b6d43aa40e9d3a6a02
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2024-07-31 01:18:26 +0500

    commit

--- cart.js
+++ cart.js
@@ -8,11 +8,14 @@ userEmailBx.innerHTML = localStorage.getItem("currentUser");
 const displayCartItems = async () => {
   let totalAm = 0;
   const cartItems = await getDocs(collection(db, "product"));
+  console.log(cartItems);
   cartItems.forEach((items) => {
-    console.log(items);
     const itemKey = items._document.key.path.segments[6];
-    console.log(itemKey);
-    totalAm += items.data().product.price * items.data().quantity;
+    if (items.data().email == localStorage.getItem("currentUser")) {
+      totalAm += items.data().product.price * items.data().quantity;
+    }else{
+      totalAm = 0.00
+    }
     console.log(totalAm.toFixed(2));
     const currentEmail = localStorage.getItem("currentUser");
     if (items.data().email == currentEmail) {
@@ -39,24 +42,35 @@ const displayCartItems = async () => {
       <button id="deleteItemBTN" value='${itemKey}' class="bg-red-600 text-white rounded p-3 w-2/5">delete</button>
         </div>
       </div>`;
-      if (!cartItems.docs) {
-        cartItemsBx.innerHTML = `<p>Cart Is Empty</p>`;
-      }
       cartItemsBx.innerHTML += itemCard;
     }
   });
-    
-    const totalAmBx = document.getElementById("totalAm");
-    
-    totalAmBx.innerHTML = `total Amount : $${totalAm.toFixed(2)}`;
-    
-    const deleteItemBTN = document.getElementById("deleteItemBTN");
-    
- deleteItemBTN &&   deleteItemBTN.addEventListener("click", async (e) => {
-      const itemId = e.target.value;
-      await deleteDoc(doc(db, "product" , itemId));
+  
+  const items = cartItems.docs
+  console.log(items);
+
+  // items.forEach((items)=>{
+  //   console.log(items);
+  //   if (!items) {
+  //     cartItemsBx.innerHTML = `<p class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Cart Is Empty</p>`;
+  //   }else{
+   
+  //   }
+  // })
+  
+
+  const totalAmBx = document.getElementById("totalAm");
+
+  totalAmBx.innerHTML = `total Amount : $${totalAm.toFixed(2)}`;
+
+  const deleteItemBTN = document.getElementById("deleteItemBTN");
+
+  deleteItemBTN &&
+    deleteItemBTN.addEventListener("click", async (e) => {
+      const itemId = e.target.value;  
+      await deleteDoc(doc(db, "product", itemId))
       console.log("deleted");
-      displayCartItems()
+      displayCartItems();
     });
 };
 
